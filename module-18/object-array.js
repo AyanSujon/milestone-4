41582e35c6af34cb75d9566bf3f21f40c7afb690
Ayan <sujon.seo.bd@gmail.com>
2025-08-06 13:18:16 +0600

    added Object-array.js file

---
+++ module-18/object-array.js
@@ -0,0 +1,20 @@
+// 18_5: Array of Objects And Access Object inside an Array
+
+
+
+const numbers = [1, 2, 43, 54, 4];
+const players = [{}, {}, {}];
+const employees = [
+    {name: 'Ashik', designation: 'Content Writer', salary: 25000},
+    {name: 'Atik', Designation: 'Developer', salary: 29000},
+    {name: 'Abdul Rahim', Designation: 'Digital Marketer', salary: 21000}
+]
+
+// how to find a value of the Array of Objects by array index value
+// console.log(employees[0]);
+console.log(employees[1].salary);
+
+
+
+
+
