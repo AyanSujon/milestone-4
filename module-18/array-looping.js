b7e1d7dbb5c807a30e535cdc25792be6ce25054f
Ayan <sujon.seo.bd@gmail.com>
2025-08-06 11:22:12 +0600

    added some code

---
+++ module-18/array-looping.js
@@ -0,0 +1,35 @@
+
+// fond out any value for an Arry 
+
+const fruits = ['Apple', 'Banana', 'Anar', 'Watermelon', 'Daav'];
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+//     console.log(fruits[i]);
+// }
+
+
+// fond out any value for an Arry by for loop
+for(let i = 0; i < fruits.length; i++){
+    // console.log(i);
+    // console.log(fruits[i]);
+}
+
+const numbers = [12, 45, 78, 145, 7, 44, 765];
+for(let j = 0; j < numbers.length; j++){
+    // console.log(numbers[j]);
+}
+
+// fond out any value for an Arry by while loop
+let k = 0;
+while(k < fruits.length){
+    // console.log(fruits[k]);
+    k++;
+}
+
+const prices = [15, 25, 58, 69, 488, 566, 54, 96, 45];
+let n = 0;
+while(n < prices.length){
+    console.log(prices[n]);
+    n++;
+}
+
