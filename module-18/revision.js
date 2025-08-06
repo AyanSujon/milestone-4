87f488ce07b63acb293152fa1d50b305d469d895
Ayan <sujon.seo.bd@gmail.com>
2025-08-06 10:29:33 +0600

    added some code

--- module-18/revision.js
+++ module-18/revision.js
@@ -1,2 +1,15 @@
 // Revision Javascript
+/*
+// 18-1: Recap: Variable, Array, Object, data Type, Loop & Comdations
+
+1. const > value not going to change.change
+2. let > Value might change
+3. var > should not use (unless exceptional cases).
+
+*/
+
+// premetive data Type
+const price = 500; //number
+const name = "kirkir ali khan"; //String
+const isPoor = false; //boolean 
 
