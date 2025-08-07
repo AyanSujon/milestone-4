842772daf73b9d12c89b64f88ba32458c24d408b
Ayan <sujon.seo.bd@gmail.com>
2025-08-07 21:17:08 +0600

    added arguments.js file

---
+++ module-19/arguments.js
@@ -0,0 +1,7 @@
+// How Function Works and Argument Vs Parameters
+
+function doubleIt (number){
+    const doubled = number * 2; 
+    console.log(number, doubled);
+}
+doubleIt(2);
--- module-19/function.js
+++ module-19/function.js
@@ -1,3 +1,13 @@
 // 19-1: Fundamentals of Functions
 
+// Function syntex: function mane(){}
 
+const price = 45;
+
+function fanOffKor(){
+    console.log('bosa thke uthe dara');
+    console.log('walk towards the switch');
+
+}
+// call the function
+fanOffKor();
