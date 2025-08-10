bc3bb1026368a1f9222de23a9bca2c217da6d743
Ayan <sujon.seo.bd@gmail.com>
2025-08-10 13:13:02 +0600

    added math.js file

---
+++ module-20/math.js
@@ -0,0 +1,22 @@
+// 20-6: Math, Abs, Round, Ceil, Floor, And Random Number
+
+// const Math = {
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+// console.log(Math.min);
+
+
+
+const min = Math.min(45, 21, 65, 99, 126, -99);
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126, -99);
+console.log(max);
+
+console.log(Math.PI);
+
+
+
+
