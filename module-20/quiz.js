c6b6f636a5d43329d10a98cbd4c610caf68121d6
Ayan <sujon.seo.bd@gmail.com>
2025-08-10 19:44:07 +0600

    added quiz.js file

---
+++ module-20/quiz.js
@@ -0,0 +1,11 @@
+// Quiz
+for( var i  = 0; i < 5; i++){
+    setTimeout(function(){
+        // console.log(i);
+    },100);
+}
+// -==================-
+const sod = Math.pow(2,3);
+console.log(sod);
+
+
