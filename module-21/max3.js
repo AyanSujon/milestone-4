55df5ac226a42cccaf9c763392d917533c94e6c1
Ayan <sujon.seo.bd@gmail.com>
2025-08-10 21:36:05 +0600

    added max3.js file

--- module-21/max3.js
+++ module-21/max3.js
@@ -1,3 +1,47 @@
 
 // find max item within 3 items
 
+// const jim = 56;
+// const tim = 89; 
+// const kim = 168;
+
+
+// if(jim > tim && jim > kim ){
+//     console.log('Jim is the ultimate boss.');
+// }
+// else if(tim > jim && tim > kim){
+//     console.log('Tim is the boss.');
+// }
+// else{
+//     console.log('Kim is the kardshians boss.');
+// }
+
+
+// inside a function find max item within 3 items
+
+function maxOfThree(num1, num2, num3){
+    // do it yourself
+    
+const jim = num1;
+const tim = num2; 
+const kim = num3;
+
+if(jim > tim && jim > kim ){
+    console.log('Jim is the ultimate boss.');
+}
+else if(tim > jim && tim > kim){
+    console.log('Tim is the boss.');
+}
+else{
+    console.log('Kim is the kardshians boss.');
+}
+
+}
+const winer = maxOfThree(330, 50, 70,);
+console.log(winer); 
+
+
+// another example
+const max = Math.max(12, 1, 56);
+console.log('Max is king Math.max ', max);
+
