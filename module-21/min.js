6172ad50e700d06735b403f3d7daabf157c769fe
Ayan <sujon.seo.bd@gmail.com>
2025-08-11 11:58:04 +0600

    added min.js file

---
+++ module-21/min.js
@@ -0,0 +1,29 @@
+// 21-4: Find the cheapest phone from an array of phone object.
+
+
+const numbers = [20000, 16000, 50000, 1000000, 12000, 30000, 350000];
+
+function getMin(numbers){
+    let min = numbers[0];
+    for(const num of numbers){
+        if(num < min){
+            min = num;
+        }
+    }
+    return min; 
+}
+
+
+const cheap = getMin(numbers);
+console.log('cheapest one is: ', cheap);
+
+
+
+
+
+
+
+
+
+
+
