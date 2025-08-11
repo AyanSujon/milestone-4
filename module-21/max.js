eb0d0813e7913c9c08c959135804ab96de7c9413
Ayan <sujon.seo.bd@gmail.com>
2025-08-11 10:31:44 +0600

    added min.js file

--- module-21/max.js
+++ module-21/max.js
@@ -20,3 +20,22 @@ const max1 =  getMax(96, 79);
 const max2 = getMax(56, 98);
 const ultimateMax = getMax(max1, max2);
 console.log('max of two is: ', ultimateMax);
+
+
+
+//////////////////////////////////////////////////
+const heights = [65, 66, 72, 78, 60, 65, 66];
+
+function getMax(numbers){
+    let max = numbers[0];
+    for(const num of numbers){
+        if(num > max){
+            max = num; 
+        }
+    }
+    return max; 
+}
+
+const max = getMax(heights);
+console.log('Max value is: ', max);
+
