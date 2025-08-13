53fca0c02ac03c4a16249eaf4407139d9c28efb0
Ayan <sujon.seo.bd@gmail.com>
2025-08-13 18:02:49 +0600

    added some code

--- sessions/validProposal.js
+++ sessions/validProposal.js
@@ -24,6 +24,24 @@ console.log(output);
 
 
 
+// another example: 
+// function validProposal(person1, person2){
+//     if(typeof person1 !== "object" || typeof person2 !== 'object'){
+//         return "Invalid";
+//     }
+//     if(person1.gender == person2.gender || Math.abs(person1.age - person2.age) > 7){
+//         return false;
+//     }
+//     return true;
+// }
+// let output = validProposal(
+// { name: "Rahul", gender: "male", age: 28 },
+// { name: "Joya", gender: "female", age: 21 }
+// );
+
+// console.log(output);
+
+
 
 
 
